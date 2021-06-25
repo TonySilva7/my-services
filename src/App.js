d9c524fe2c320e25ecbad6af607da4a7dd304341
TonySilva7 <tony.deve7oper@gmail.com>
2021-06-24 23:11:24 -0300

    chore: connect firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from "./services/firebaseConnection";
 
 function App() {
   return (
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyD9NnPrTUmHJsbipQ8sFkhP9SZVzO5Gbx0",
+  authDomain: "my-services-547b6.firebaseapp.com",
+  projectId: "my-services-547b6",
+  storageBucket: "my-services-547b6.appspot.com",
+  messagingSenderId: "182485226720",
+  appId: "1:182485226720:web:bf0972fa96729ce5de2c85",
+  measurementId: "G-C58EBCZ2PE"
+};
+
+if (!firebase.app.length) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
