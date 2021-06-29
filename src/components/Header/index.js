cc9cbf0dc4f1b7e7502c6522bf8a38aebd884578
TonySilva7 <tony.deve7oper@gmail.com>
2021-06-29 12:35:34 -0300

    style: responsive design Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,35 @@
 import { useContext } from 'react';
+import { FiHome, FiSettings, FiUser } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+import { ReactComponent as Avatar } from '../../assets/avatar.svg';
 import { AuthContext } from '../../contexts/auth';
 import { SideBar } from './styles';
-import { ReactComponent as Avatar} from '../../assets/avatar.svg'
-import { Link } from 'react-router-dom';
+// import { IoHome } from 'react-icons/all';
 
 export default function Header () {
   const { user } = useContext(AuthContext);
 
   return (
     <SideBar>
-      { user.avatarUrl === null ? <Avatar alt="avatar"/> : "imagem"}
-      <Link>Chamados</Link>
+      <div>
+        { user.avatarUrl === null ? <Avatar alt="avatar"/> : "imagem" }
+      </div>
 
-      <span>{ user.name }</span>
+      <Link to="/dashboard">
+        <FiHome color="#fff" size={ 24 }/>
+        Chamados
+      </Link>
+
+      <Link to="/dashboard">
+        <FiUser color="#fff" size={ 24 }/>
+        Clientes
+      </Link>
+
+      <Link to="/dashboard">
+        <FiSettings color="#fff" size={ 24 }/>
+        Configurações
+      </Link>
+
     </SideBar>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,100 @@
 import styled from 'styled-components';
+import img from '../../assets/cover.png';
 
-export const SideBar = styled.div`
-  
+export const SideBar = styled.aside`
+
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  min-width: 200px;
+  background-color: var(--primary-color);
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 150px;
+
+    background-color: var(--primary-color);
+    background-image: url(${ img });
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+
+  & > div > svg {
+    width: 90px;
+    height: 90px;
+    object-fit: cover;
+    -webkit-filter: drop-shadow(2px 3px 6px #121212);
+    filter: drop-shadow(2px 3px 6px #121212);
+  }
+
+  & > a {
+    display: flex;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.7);
+    padding: 16px;
+    text-decoration: none;
+    transition: all .5s ease;
+  }
+
+  & > a:hover {
+    background-color: #171624;
+    color: #fff;
+  }
+
+  & > a > svg {
+    margin-right: .7em;
+  }
+
+
+  @media screen and (max-width: 700px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: auto;
+    position: relative;
+
+    & > div {
+      display: none;
+    }
+
+    & > a {
+      flex-direction: row;
+    }
+
+  }
+
+  @media screen and (max-width: 400px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: auto;
+    position: relative;
+
+    & > div {
+      display: none;
+    }
+
+    & > a {
+      flex-direction: row;
+      text-align: center;
+    }
+
+    & > a > svg {
+      display: none;
+    }
+
+  }
+
 `;
